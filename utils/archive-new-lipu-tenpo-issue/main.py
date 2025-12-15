08560954c072fb46355829c0ab49289f6144faac
vivi <ubersweet@duck.com>
2025-12-15 16:04:29 +0100

    update comments

--- utils/archive-new-lipu-tenpo-issue/main.py
+++ utils/archive-new-lipu-tenpo-issue/main.py
@@ -5,20 +5,23 @@ import datetime as dt
 
 # lipu tenpo publishes a set of markdown files every 1-2 months,
 # so it is helpful to do this almost entirely automatically.
-# Change the consts to match the current date and issue.
+# Change the consts to match the current date and issue,
+# and the path of your local liputenpo.org repo.
 #
 # After dumping, make sure to go through manually and check
 # the metadata for errors or potential additions, e.g.:
 #  - references to original works;
 #  -  list of proofreaders;
 #  -  wikisource link.
+#
+# The main collection file currently needs to be updated manually.
 
 # === Usage ===
 LIPU_TENPO_TITLE = "lon"
 LIPU_TENPO_NANPA = "0034"
 DATE = "2025-11-09"
-
 LIPU_TENPO_DIRECTORY = "../../../liputenpo.org/content/md/"
+
 YEAR = int(DATE[:4])
 MONTH = int(DATE[5:7])
 DAY = int(DATE[8:10])
